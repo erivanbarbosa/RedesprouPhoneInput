d356ce4b6dabf72f2f9135a4323bf5cf046f739a
Erivan <erivan.nas@gmail.com>
2019-06-23 08:42:55 -0300

    control value acessor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'redesprou-phone-input';
   telephone = '';
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.form = this.fb.group({
+      telephone: ['62993718452'],    
+    });
+  }
+
 
 
   printTelephone(event) {
--- src/app/app/phone-input/phone-input.component.ts
+++ src/app/app/phone-input/phone-input.component.ts
@@ -1,18 +1,54 @@
-import { Component, OnInit, HostListener, ElementRef, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, HostListener, ElementRef, Output, EventEmitter, forwardRef } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+const noop = () => { };
 
 @Component({
   selector: 'app-phone-input',
   templateUrl: './phone-input.component.html',
-  styleUrls: ['./phone-input.component.css']
+  styleUrls: ['./phone-input.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PhoneInputComponent),
+      multi: true
+    }]
+
 })
-export class PhoneInputComponent implements OnInit {
+
+export class PhoneInputComponent implements OnInit, ControlValueAccessor {
+
 
   public openList = false;
   countryCode = '+55';
   form: FormGroup;
   flag = 'br';
   @Output() onTelephoneChanged = new EventEmitter<string>();
+  innerValue = 'teste';
+
+  
+  private onTouchedCallback: () => void = noop;
+  private onChangeCallback: (_: any) => void = noop;
+  disabled: boolean;
+
+  writeValue(value: any) {
+    if(value) {
+      this.innerValue = value;
+      this.form.get('telephone').setValue(value);
+      this.onChangeCallback(this.innerValue);
+    } 
+  }
+
+  registerOnChange(fn: any) {
+    this.onChangeCallback = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouchedCallback = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled; 
+  }
 
   constructor(private fb: FormBuilder,
     private eRef: ElementRef ) { }
@@ -52,11 +88,14 @@ export class PhoneInputComponent implements OnInit {
     if(this.form.valid) {
       const code = this.form.get('countryCode').value;
       const phone = this.form.get('telephone').value
+      this.innerValue =  code + phone;
+      this.onChangeCallback(this.innerValue);
       this.onTelephoneChanged.emit( code + phone);
     } else {
       this.onTelephoneChanged.emit('');
     }
   }
+
 
 
 
