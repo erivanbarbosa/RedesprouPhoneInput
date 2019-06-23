42b1f8fcb4814989c6ed92ef4ecd38146dc1ed72
Erivan <erivan.nas@gmail.com>
2019-06-23 10:43:45 -0300

    Refatoração

--- src/app/app/TelephoneUtils.ts
+++ src/app/app/TelephoneUtils.ts
@@ -1,53 +1,25 @@
-import { countryCodes, countryCodesList } from './countryCodes';
+import { countryCodesList } from './countryCodes';
 import { CountryCodeData } from './CountryCodeData';
 
 export class TelephoneUtils {
     constructor() { }
 
     public getTelephoneCountryCode(telephone: string): string {
-        const country = countryCodesList.find(ct => this.contains(telephone, ct));
-        if(country)
-            return country.code;
-        else
-            return null;
-    }
-
-    private contains(telephone: string, ct: { "name": string; "iso2": string; "code": string; }): boolean {
-        const result =  telephone.includes(ct.code);
-        return result;
+        const country = this.findCountryByTelephone(telephone);
+        return country ? country.code : null;
     }
 
     public getCountryCodeData(telephone: string): CountryCodeData {
-        const country = countryCodesList.find(ct => this.contains(telephone, ct));
-        if(country)
-            return country
-        else
-            return null;
+        const country = this.findCountryByTelephone(telephone);
+        return country ? country : null;
     }
 
-
-
-    public static getPhoneMask(phone: string): string {
-        phone = phone ? phone.replace(/\s/g, '') : "";
-        if (phone != null && phone.length > 12)
-            return '+55 (00) 0 0000-0000';
-        else
-            return '+55 (00) 0000-00009';
+    private findCountryByTelephone(telephone: string) {
+        return countryCodesList.find(country => this.contains(telephone, country));
     }
 
-
-    public static getPhoneMaskPT() {
-        return '(00) 0 0000-0000';
+    private contains(telephone: string, country: CountryCodeData): boolean {
+        return telephone.includes(country.code);
     }
-
-    public static getPhoneMaskFix() {
-        return '(00) 0000-0000';
-    }
-
-    public static getPhoneMaskEN() {
-        return '(000) 000-0000';
-    }
-
-
 }
 
